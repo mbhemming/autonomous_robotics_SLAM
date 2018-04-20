5eaba46335d0505d3eb4d1523ea4a238504dd35d
Gary Hubley <gary.hubley@dal.ca>
2018-04-20 19:37:17 -0300

    incomplete copy of angle sweep

--- Python/ev3Functions.py
+++ Python/ev3Functions.py
@@ -1,3 +1,8 @@
+from enum import Enum
+class ROTATION(Enum):
+	CW = 1
+	CCW = 2
+    
 def ResetMotorPositions():
 	global motorRight
 	global motorLeft
@@ -149,3 +154,53 @@ def calculate_theta( dest_x, dest_y ):
 
 def calculate_dist( dest_x, dest_y ): 
 	return math.sqrt( math.pow( current_x - dest_x, 2 ) + math.pow( current_y - dest_y, 2 ) )
+	
+def rotate_sensor( angle, direction = ROTATION.CW ):
+function [ current_sensor_angle ] = angle_sweep( rotation_initial, sweep_angle_max, angle_increment, j, ...
+motorRotate )
+
+	%gear ratio of sensor
+	gear_ratio = 40/8;
+	rotate_speed = 20;
+
+motorRotate.Speed = rotate_speed;
+delta_rotation = 0;
+
+%Move sensor to furthest angle for sweep
+if ( j == 1 )
+    rotate_angle = sweep_angle_max;
+    
+    while( abs( delta_rotation ) < rotate_angle * gear_ratio );
+        rotation = readRotation( motorRotate );
+        delta_rotation = abs( rotation - rotation_initial );
+        start( motorRotate );    
+    end
+    
+end
+    
+stop( motorRotate )
+
+if ( j > 1 )
+    delta_rotation = 0;
+    motorRotate.Speed = -rotate_speed;
+
+    rotation_initial_temp = readRotation(motorRotate);
+
+    while ( abs(delta_rotation) < angle_increment*gear_ratio )
+        rotation = readRotation(motorRotate);
+        delta_rotation = abs( rotation - rotation_initial_temp );
+        start( motorRotate );    
+    end
+
+    stop( motorRotate ) 
+
+end
+    
+current_sensor_angle = (readRotation(motorRotate) - rotation_initial)/gear_ratio;
+if current_sensor_angle < 0
+    current_sensor_angle = 360+current_sensor_angle;
+else
+    
+end
+
+end
