72697c48b1734c6263491a18d3ead72a99ea860e
Matthew Hemming <mbhemming@icloud.com>
2018-04-20 20:23:49 -0300

    functions to convert to Python are in dir=MATLAB
    Other files that don't need to be converted are in sub directories

--- MATLAB/get_dimensions_a_trejectory_could_hit_wall2.m
+++ MATLAB/get_dimensions_a_trejectory_could_hit_wall2.m
@@ -1,6 +1,8 @@
 function [ x_min_search,y_min_search,   x_max_search,y_max_search ] = get_dimensions_a_trejectory_could_hit_wall2( x_robot,y_robot,    dir_x_component, dir_y_component,   x_board_top_corner,y_board_top_corner )
 %GET_DIMENSIONS_A_TREJECTORY_COULD_HIT_WALL Summary of this function goes here
 %   Detailed explanation goes here
+% Given the current location and trejectory, there are only certain x and y values
+% the the current trejectory could hit the enviorment boundries
     
     x_board_top_corner=convert_inches_to_EV3_units(x_board_top_corner);
     y_board_top_corner=convert_inches_to_EV3_units(y_board_top_corner);
