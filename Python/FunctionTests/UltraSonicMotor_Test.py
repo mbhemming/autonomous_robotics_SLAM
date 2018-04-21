d47e7103da858174df9e871ab0e45f03dc5aef4d
robot <robot@ev3dev.ca>
2018-04-21 19:20:47 +0000

    Modified unit tests to reflect new functionality

--- Python/FunctionTests/UltraSonicMotor_Test.py
+++ Python/FunctionTests/UltraSonicMotor_Test.py
@@ -5,25 +5,24 @@ import ev3dev.ev3 as ev3
 import ev3Functions as func
 from time import sleep
 
-
 motorUltraSonic = ev3.MediumMotor()
 assert motorUltraSonic.connected
 
 motorUltraSonic.position_sp = 0
 
-func.rotate_sensor( motorUltraSonic, 45 )
+func.SetSensorAngle( motorUltraSonic, 45 )
 
 motorUltraSonic.wait_until_not_moving()
 
 sleep(2)
 
-func.rotate_sensor( motorUltraSonic, 90, func.ROTATION.CW )
+func.SetSensorAngle( motorUltraSonic, -45 )
 
 motorUltraSonic.wait_until_not_moving()
 
-
 sleep(2)
 
-func.rotate_sensor( motorUltraSonic, 45 )
+func.ResetSensorAngle( motorUltraSonic )
+
 motorUltraSonic.wait_until_not_moving()
 
