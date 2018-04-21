3ed9c6b9cd6cf2dbb21ae442f9aa09de7b10d8a3
robot <robot@ev3dev.ca>
2018-04-21 22:37:03 +0000

    added assertions to robot constructor

--- Python/Objects/Robot.py
+++ Python/Objects/Robot.py
@@ -9,6 +9,11 @@ class Robot:
 		self.MUltra = motorUltra
 		self.SUltra = sensorUltra
 		self.STouch = sensorTouch
+		assert self.MLeft.connected, "Left motor not connected"
+		assert self.MRight.connected, "Right motor not connected" 
+		assert self.MUltra.connected, "Ultrasonic (medium) motor not connected"
+		assert self.SUltra.connected, "Ultrasonic sensor not connected"
+		assert self.STouch.connected, "Touch sensor not connected"
 	
 	def __str__(self):
 		return str( self.Pose ) + "\nMotors: \n  Left: " +\
