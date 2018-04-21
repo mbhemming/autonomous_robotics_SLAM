40f7caba280a1fc768f850f0a8a33061a527c55f
robot <robot@ev3dev.ca>
2018-04-21 18:40:32 +0000

    added globals and enums, changed functions to camelcase

--- Python/ev3Functions.py
+++ Python/ev3Functions.py
@@ -1,18 +1,33 @@
+#!/usr/bin/env python3
+import sys
+sys.path.insert(0, '/home/robot/Capstone/Python/Objects')
+sys.path.insert(0, '/home/robot/Capstone/Python/')
+import ev3dev.ev3 as ev3
+from time import sleep
 from enum import Enum
+
+######################ENUMS#######################
 class ROTATION(Enum):
 	CW = 1
 	CCW = 2
-    
-def ResetMotorPositions():
-	global motorRight
-	global motorLeft
-	motorRight.position = 0
-	motorLeft.position = 0
+##################################################
+
+#####################GLOBALS######################
+GEAR_RATIO = 40/8
+ROTATION_SPEED = 200
+
+DRIVE_SPEED = 125
+SPEED_CONSTANT_CM = 0.007958 # (cm/ms) 
+SPEED_CONSTANT_IN = 0.02021332 # (in/ms) 
+##################################################
+
+def ResetMotorPositions( motor1, motor2 ):
+	motor1.position_sp = 0
+	motor2.position_sp = 0
 	
 # This function turns the robot to a specified angle using only 1 wheel. 
 # This does not look at color values while turning.
-def TurnDeg( angle, wheel ):
-	speed = 125 # Arbitrary number determined by trial and error
+def TurnOneWheelDeg( angle, motorLeft, motorRight, wheel ):
 	
 	# 2.1818 is the ratio of the wheel radius to the robot radius. 
 	# The formula is the arc length formula.
@@ -20,125 +35,64 @@ def TurnDeg( angle, wheel ):
 	
 	if wheel == 'left':
 		#turn CW
-		motor_A.stop( stop_action = "hold" )
-		motor_D.run_to_rel_pos( position_sp = position, speed_sp = speed, stop_action = "hold" )
+		motorRight.stop( stop_action = "hold" )
+		motorLeft.run_to_rel_pos( position_sp = position, \
+								  speed_sp = DRIVE_SPEED, \
+								  stop_action = "hold" )
 	elif wheel == 'right':
 		#turn CCW
-		motor_D.stop( stop_action = "hold" )
-		motor_A.run_to_rel_pos( position_sp = position, speed_sp = speed, stop_action = "hold" )
+		motorLeft.stop( stop_action = "hold" )
+		motorRight.run_to_rel_pos( position_sp = position, \
+								   speed_sp = DRIVE_SPEED, \
+								   stop_action = "hold" )
 	else:
 		# *Sanity
 		print( "invalid wheel", file = sys.error )
 		
 # This function turns the robot to a specified angle using both wheels. 
 # This does not look at color values while turning.		
-def TurnDegTwoWheel( angle ):
-	speed = 125 	# Arbitrary number determined by trial and error
-	
+def TurnTwoWheelDeg( angle, motorLeft, motorRight ):
 	# 2.1818 is the ratio of the wheel radius to the robot radius. 
 	# The formula is the arc length formula.
 	position = 2.1818 *  angle
 	
 	# run the motors.
-	motor_A.run_to_rel_pos(position_sp = ((-1.0) *position), speed_sp = speed, stop_action = "hold" )
-	motor_D.run_to_rel_pos( position_sp = position, speed_sp = speed, stop_action = "hold" )
-	
-# This function turns the robot 90 degrees CCW while watching the colour values from the sensor. If the colour value
-# is within the hysteresis band, the motor is stopped and a True value is returned. If the value never reaches the 
-# hysteresis band, the robot is returned to its original position and a False value is returned. 
-def CCW_sweep():
-	global value
-	
-	print(" CCW Sweep.\n")
-	
-	TurnDeg( angle = 100, wheel = 'right' )
-	
-	while motor_A.is_running:
-		# Read colour sensor.
-		value = cs.value()
-		
-		if value < hyst_high and value > hyst_low:
-			# Tape found
-			motor_A.stop()
-			return True
-		
-		sleep( 0.01 ) # sanity
-	
-	# Tape not found
-	TurnDeg( angle = -100, wheel = 'right' )
-	
-	motor_A.wait_while('running')
-	return False
+	motorRight.run_to_rel_pos( position_sp = -position, speed_sp = DRIVE_SPED, \
+							stop_action = "hold" )
+	motorLeft.run_to_rel_pos( position_sp = position, speed_sp = DRIVE_SPEED, \
+						      stop_action = "hold" )
 
-# This function turns the robot 90 degrees CW while watching the colour values from the sensor. If the colour value
-# is within the hysteresis band, the motor is stopped and a True value is returned. If the value never reaches the 
-# hysteresis band, the robot is returned to its original position and a False value is returned. 
-def CW_sweep():
-	global value
-	
-	print(" CW Sweep.\n")
-	
-	TurnDeg( angle = 100, wheel = 'left' )
-	
-	while motor_D.is_running:
-		# Read colour sensor.
-		value = cs.value()
-		
-		if value < hyst_high and value > hyst_low:
-			# Tape found
-			motor_D.stop()
-			return True
-		
-		sleep( 0.01 ) # sanity
-		
-	# Tape not found
-	TurnDeg( angle = -100, wheel = 'left' )
-	
-	motor_D.wait_while('running')
-	return False
+# Travel straight for a distance measured in CM. 
+def StraightDistCM( dist, motorLeft, motorRight ):
+	dtMilli =  dist / ( SPEED_CONSTANT_CM )
+	motorLeft.run_timed( time_sp = dtMilli, speed_sp = DRIVE_SPEED )	
+	motorRight.run_timed( time_sp = dtMilli, speed_sp = DRIVE_SPEED )
 
-# This function drives the robot straight a specified number of CM while watching the colour values from the sensor. 
-# If the colour value is within the hysteresis band, the motors are stopped and a True value is returned. If the value 
-# never reaches the hysteresis band, a False value is returned. 
-def straight_cm_sweep( dist ):
-	global value
-	
-	print(" Straight search along %d cm.\n" % dist)	
-	
-	speed = 100 # Arbitrary speed chosen by trial and error.
-	
-	#20.83 is derived from arc length formula
-	motor_A.run_to_rel_pos( position_sp = dist * 20.83, speed_sp = speed, stop_action = "hold" )
-	motor_D.run_to_rel_pos( position_sp = dist * 20.83, speed_sp = speed, stop_action = "hold" )
-	
-	while motor_A.is_running:
-		# Read colour sensor
-		value = cs.value()
-		
-		sleep(0.01) # sanity
-		
-		if value < hyst_high:
-			# Tape found, stop motors
-			motor_A.stop()
-			motor_D.stop()
-			return True
-			
-	#tape not found, motors already stopped. 
-	return False
-	
-def straight_cm( dist ):
-	dtMilli =  dist / ( SPEED_CONSTANT )
-	motor_A.run_timed( time_sp = dtMilli, speed_sp = speed )	
-	motor_D.run_timed( time_sp = dtMilli, speed_sp = speed )
-#	if( dtMilli != 0.0 ):
-	motor_A.wait_while( 'running', dtMilli )
-	motor_D.wait_while( 'running', dtMilli )
+	motorLeft.wait_while( 'running', dtMilli )
+	motorRight.wait_while( 'running', dtMilli )
 	if( doprint == 1 ):
 		print( "Straight:" )
 		print( "  dist: " + str( dist ) )
 		print( "  dt: " + str( dtMilli ) ) 
 
-def calculate_theta( dest_x, dest_y ):
+# Travel straight for a distance measured in IN. 
+def StraightDistIN( dist, motorLeft, motorRight ):
+	dtMilli =  dist / ( SPEED_CONSTANT_IN )
+	motorLeft.run_timed( time_sp = dtMilli, speed_sp = DRIVE_SPEED )	
+	motorRight.run_timed( time_sp = dtMilli, speed_sp = DRIVE_SPEED )
+
+	motorLeft.wait_while( 'running', dtMilli )
+	motorRight.wait_while( 'running', dtMilli )
+	if( doprint == 1 ):
+		print( "Straight:" )
+		print( "  dist: " + str( dist ) )
+		print( "  dt: " + str( dtMilli ) ) 
+
+# Calculate angle between current robot angle and desired position
+# Arguments:
+#     dest: destination point as Point object
+#     pose: robot pose as Pose object
+def calculate_theta( dest_x, dest_y, cur_theta, cur_x, cur_y ):
 	# formula found on math stack exchange
 	# calc current unit vector of robot using current theta
 	x1 = math.cos( math.radians( current_theta ) ) 
@@ -155,13 +109,32 @@ def calculate_theta( dest_x, dest_y ):
 def calculate_dist( dest_x, dest_y ): 
 	return math.sqrt( math.pow( current_x - dest_x, 2 ) + math.pow( current_y - dest_y, 2 ) )
 	
-def rotate_sensor( motor, angle, direction = ROTATION.CCW ):
-	#gear ratio of sensor
-	gear_ratio = 40/8
-	rotate_speed = 200
+def set_sensor_angle( motor, angle, direction = ROTATION.CCW ):
 
 	if( direction.value == ROTATION.CW.value ):
 		angle = -angle
 	
-	motor.run_to_rel_pos( position_sp = angle * gear_ratio, speed_sp = rotate_speed )
+	motor.run_to_rel_pos( position_sp = angle * GEAR_RATIO, speed_sp = ROTATION_SPEED )
 
+def zero_sensor_rotation( motor ):
+	motor.run_to_abs_pos( position_sp = 0, speed_sp = 200 )
+	motor.position_sp = 0
+	return 0
+
+def angle_sweep_for_sensor_readings( rotation_initial, sweep_angle_max, angle_increment, motorRotate, number_of_discrete_sensor_angles, j ):
+	#initialize delta_rotation
+	delta_rotation = 0;
+	
+	#Move sensor to furthest angle for sweep
+	if( j == 1 ):
+		new_pos = rotate_sensor( motor, angle_max )
+    
+	# Now that max angle has been reached begins sweeping through angles by angle increments 
+	if( j > 1 ):
+		new_pos = rotate_sensor( motor, angle_increment, ROTATION.CW )
+
+	# This step returns the sensor back to the forward (zero'd postion) 
+	if( j > number_of_discrete_sensor_angles ):
+		new_pos = zero_sensor_rotation( motor )
+	
+	return new_pos
