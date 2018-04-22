e1922b552b93f7ce9e4e36fc416c262ec4b67968
robot <robot@ev3dev.ca>
2018-04-22 22:42:50 +0000

    Modified point constructor and implicit string conversion

--- Python/Objects/Point.py
+++ Python/Objects/Point.py
@@ -1,8 +1,8 @@
 class Point:
 
-	def __init__( self, x = 0., y = 0. ):
+	def __init__( self, x, y ):
 		self.x = x
 		self.y = y
 	
 	def __str__(self):
-		return "( %s, %s )" % ( self.x, self.y )
+		return "( " + str( self.x ) + ", " + str( self.y ) + " )"
