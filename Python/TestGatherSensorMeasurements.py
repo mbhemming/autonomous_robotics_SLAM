6b82c7ad10f1d6219c545a1bcc6469abfbfd51b2
robot <robot@ev3dev.ca>
2018-04-22 19:21:48 +0000

    Modified stationary test for measurement gathering

--- Python/TestGatherSensorMeasurements.py
+++ Python/TestGatherSensorMeasurements.py
@@ -16,34 +16,16 @@ from RobotPose import RobotPose
 from time import sleep
 import time
 ##################INITIALIZATION##################
-# Setup the wheel motors
-motorRight = ev3.LargeMotor('outA')
-motorLeft = ev3.LargeMotor('outD')
-motorRight.stop()
-motorLeft.stop()
-
-# Setup motor controlling Ultrasonic sensor
-motorUltra = ev3.MediumMotor()
-motorUltra.stop()
-
-# Setup ultrasonic sensor
-# US-DIST-IN range is 0-1003 (1003 is 255 cm)
-sensorUltra = ev3.UltrasonicSensor()
-sensorUltra.mode = 'US-DIST-IN'
-
-# Setup touch sensor
-sensorTouch = ev3.TouchSensor()
-
 start = RobotPose( Point( 0, 0 ), 90 )
 
-bot = Robot( start, motorLeft, motorRight, motorUltra, sensorUltra, sensorTouch )
+bot = Robot( start )
 print( bot )
 ev3.Sound.beep().wait()
 sleep(0.5)
 ##################################################
 timestr = time.strftime("%Y%m%d-%H%M%S")
 with open(timestr+'_TestOutput.csv', 'w') as csvfile:
-	data = np.array(GatherSensorMeasurements(50,180,5,sensorUltra,motorUltra))
+	data = np.array(GatherSensorMeasurements(50,180,5,bot.SUltra,bot.MUltra))
 	printer.AppendToCsv(csvfile, bot, data)
 	
 
