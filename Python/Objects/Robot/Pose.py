864cf35c762cd30091719e976f2c390fd83d96c1
robot <robot@ev3dev.ca>
2018-04-23 18:39:09 +0000

    Fixed added implementation of constructors

--- Python/Objects/Robot/Pose.py
+++ Python/Objects/Robot/Pose.py
@@ -1,23 +1,23 @@
 import sys
 sys.path.append( '../' )
 
-import Point 
+from Point import Point 
     
-class Pose( Point.Point ):
+class Pose( Point ):
     
     def __init__( self, x=float('nan'), y=float('nan'), theta=float('nan') ):
-        if isinstance(x,list) or  isinstance(x,tuple):
-            self.Theta = x[2]
-            super().__init__( x[0], x[1] )
-        elif isinstance(x,Pose):
+        if isinstance( x, list ) or  isinstance( x, tuple ):
+            self.Theta = x[ 2 ]
+            super().__init__( x[ 0 ], x[ 1 ] )
+        elif isinstance( x, Pose ):
             self.Theta = x.Theta
-            super().__init__(x.x, x.y)
-        elif isinstance(x,Point):
-            self.Theta = theta
-            super().__init__(x.x, x.y)
+            super().__init__( x.x, x.y )
+        elif isinstance( x, Point ):
+            self.Theta = y
+            super().__init__( x.x, x.y )
         else:
             self.Theta = theta
-            super().__init__(x,y)
+            super().__init__( x, y )
     
     def __str__(self):
         return "Pos: " + super().__str__() + "\nTheta: " + str( self.Theta )
